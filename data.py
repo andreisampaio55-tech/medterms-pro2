7b3e341e09f624ee02692fcc8a87f11a4a09751f
andreisampaio55-tech <andreisampaio55@gmail.com>
2026-04-26 17:27:39 -0300

    Create data.py

---
+++ data.py
@@ -0,0 +1,9 @@
+termos = [
+    {"termo": "Dispneia", "area": "Pneumologia",
+     "significado": "Falta de ar",
+     "exemplo": "Paciente com DPOC."},
+
+    {"termo": "Icterícia", "area": "Gastro",
+     "significado": "Coloração amarelada da pele",
+     "exemplo": "Hepatite viral."}
+]
